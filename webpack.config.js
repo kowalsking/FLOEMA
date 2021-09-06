ac7c03c6d9be496c182d5be2e66a986060b76a99
kowalsking <kovvalsking@gmail.com>
2021-09-06 23:33:30 +0200

    Webpack rest

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const webpack = require('webpack')
+const path = require('path')
+
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const IS_DEVELOPLMENT = process.env.NODE_ENV === 'dev'
+const dirApp = path.join(__dirname, 'app')
+const dirAssets = path.join(__dirname, 'assets')
+const dirStyles = path.join(__dirname, 'styles')
+
+console.log(dirApp, dirAssets, dirStyles)
