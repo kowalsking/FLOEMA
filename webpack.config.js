25e1664877b40edf170e1d2822cf70372c5fd824
kowalsking <kovvalsking@gmail.com>
2021-09-07 22:45:53 +0200

    Entry point for js

--- webpack.config.js
+++ webpack.config.js
@@ -7,3 +7,10 @@ const IS_DEVELOPLMENT = process.env.NODE_ENV === 'dev'
 const dirApp = path.join(__dirname, 'app')
 const dirAssets = path.join(__dirname, 'assets')
 const dirStyles = path.join(__dirname, 'styles')
+
+
+module.exports = {
+  entry: [
+    path.join(dirApp, 'index.js')
+  ]
+}
