e1c14860c5d628957442e5e2053a4af31a04920c
kowalsking <kovvalsking@gmail.com>
2021-09-12 23:16:53 +0200

    Setup backend & prismic communication

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const path = require('path')
 const port = 3000
+
+const Prismic = require('@prismicio/client')
+const PrismicDOM = require('prismic-dom')
+
+const initApi = req => {
+  return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req
+  })
+}
+
+const handleLinkResolver = (doc) => {
+  return '/'
+}
+
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: handleLinkResolver
+  }
+
+  res.locals.PrismicDOM = PrismicDOM
+
+  next()
+})
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
@@ -11,7 +38,17 @@ app.get('/', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-  res.render('pages/about')
+  initApi(req).then(api => {
+    api.query(Prismic.Predicates.any('document.type', ['about', 'meta'])).then(response => {
+      const { results } = response
+      const [about, meta] = results
+      console.log(about, meta)
+      res.render('pages/about', {
+        about,
+        meta
+      })
+    })
+  })
 })
 
 app.get('/detail/:uid', (req, res) => {
