a277095d76e2b5d542f77c3553a0fefca1c267db
kowalsking <kovvalsking@gmail.com>
2022-05-07 17:42:23 +0300

    vertical infinity scroll

--- app/components/Canvas/Home.js
+++ app/components/Canvas/Home.js
@@ -67,6 +67,10 @@ export default class {
     this.galleryBounds = this.galleryElement.getBoundingClientRect()
 
     this.sizes = event.sizes
+    this.gallerySizes = {
+      width: this.galleryBounds.width / window.innerWidth * this.sizes.width,
+      height: this.galleryBounds.height / window.innerHeight * this.sizes.height
+    }
   }
 
   onTouchDown({ x, y }) {
@@ -101,23 +105,45 @@ export default class {
       this.x.direction = 'left'
     }
 
-    this.galleryWidth = this.galleryBounds.width / window.innerWidth * this.sizes.width
+    if (this.scroll.y < this.y.current) {
+      this.y.direction = 'top'
+    } else if (this.scroll.y > this.y.current) {
+      this.y.direction = 'bottom'
+    }
 
     this.scroll.x = this.x.current
     this.scroll.y = this.y.current
 
     map(this.medias, media => {
+      const scaleX = media.mesh.scale.x / 2
+
       if (this.x.direction === 'left') {
-        const x = media.mesh.position.x + media.mesh.scale.x / 2
+        const x = media.mesh.position.x + scaleX
 
         if (x < - this.sizes.width / 2) {
-          media.extra.x += this.galleryWidth
+          media.extra.x += this.gallerySizes.width
         }
       } else if (this.x.direction === 'right') {
-        const x = media.mesh.position.x - media.mesh.scale.x / 2
+        const x = media.mesh.position.x - scaleX
 
         if (x > this.sizes.width / 2) {
-          media.extra.x -= this.galleryWidth
+          media.extra.x -= this.gallerySizes.width
+        }
+      }
+
+      const scaleY = media.mesh.scale.y / 2
+
+      if (this.y.direction === 'top') {
+        const y = media.mesh.position.y + scaleY
+
+        if (y < - this.sizes.height / 2) {
+          media.extra.y += this.gallerySizes.height
+        }
+      } else if (this.y.direction === 'bottom') {
+        const y = media.mesh.position.y - scaleY
+
+        if (y > this.sizes.width / 2) {
+          media.extra.y -= this.gallerySizes.height
         }
       }
 
