12b42962144acd8bb661c6adbcdb38937b721ecc
kowalsking <kovvalsking@gmail.com>
2022-05-14 23:44:23 +0300

    Refactoring

--- app/components/Canvas/Collections/index.js
+++ app/components/Canvas/Collections/index.js
@@ -13,28 +13,19 @@ export default class {
     this.mediasElements = document.querySelectorAll('.home__gallery__media__image')
     this.sizes = sizes
 
-    this.x = {
+    this.scroll = {
       current: 0,
       target: 0,
       lerp: 0.1
-    }
-
-    this.y = {
-      current: 0,
-      target: 0,
-      lerp: 0.1
-    }
-
-    this.scrollCurrent = {
-      x: 0,
-      y: 0
     }
 
     this.scroll = {
-      x: 0,
-      y: 0
+      current: 0,
+      target: 0,
+      start: 0,
+      lerp: 0.1,
+      velocity: 1
     }
-
     this.createGeometry()
     this.createGallery()
 
@@ -79,39 +70,29 @@ export default class {
     this.galleryBounds = this.galleryElement.getBoundingClientRect()
 
     this.sizes = event.sizes
-    this.gallerySizes = {
-      width: this.galleryBounds.width / window.innerWidth * this.sizes.width,
-      height: this.galleryBounds.height / window.innerHeight * this.sizes.height
-    }
+    this.width = this.galleryBounds.width / window.innerWidth * this.sizes.width
 
-    this.scroll.x = this.x.target = 0
-    this.scroll.y = this.y.target = 0
+    this.scroll.last = this.scroll.target = 0
 
     map(this.medias, media => media.onResize(event, this.scroll))
   }
 
   onTouchDown({ x, y }) {
-    this.scrollCurrent.x = this.scroll.x
-    this.scrollCurrent.y = this.scroll.y
+    this.scroll.last = this.scroll.current
   }
 
   onTouchMove({ x, y }) {
-    const xDistance = x.start - x.end
-    const yDistance = y.start - y.end
+    const distance = x.start - x.end
 
-    console.log('1')
-
-    this.x.target = this.scrollCurrent.x - xDistance
-    this.y.target = this.scrollCurrent.y - yDistance
+    this.scroll.target = this.scroll.last - distance
   }
 
   onTouchUp({ x, y }) {
 
   }
 
-  onWheel({ pixelX, pixelY }) {
-    this.x.target += pixelX
-    this.y.target += pixelY
+  onWheel({ pixelX }) {
+    this.scroll.target += pixelX
   }
 
   /**
@@ -120,58 +101,30 @@ export default class {
   update() {
     if (!this.galleryBounds || this.onResizing) return
 
-    this.x.current = GSAP.utils.interpolate(this.x.current, this.x.target, this.x.lerp)
-    this.y.current = GSAP.utils.interpolate(this.y.current, this.y.target, this.y.lerp)
+    this.scroll.current = GSAP.utils.interpolate(this.scroll.current, this.scroll.target, this.scroll.lerp)
 
-    if (this.scroll.x < this.x.current) {
-      this.x.direction = 'right'
-    } else if (this.scroll.x > this.x.current) {
-      this.x.direction = 'left'
+    if (this.scroll.last < this.scroll.current) {
+      this.scroll.direction = 'right'
+    } else if (this.scroll.last > this.scroll.current) {
+      this.scroll.direction = 'left'
     }
 
-    if (this.scroll.y < this.y.current) {
-      this.y.direction = 'top'
-    } else if (this.scroll.y > this.y.current) {
-      this.y.direction = 'bottom'
-    }
-
-    this.scroll.x = this.x.current
-    this.scroll.y = this.y.current
+    this.scroll.last = this.scroll.current
 
     map(this.medias, media => {
       const scaleX = media.mesh.scale.x / 2
 
-      if (this.x.direction === 'left') {
+      if (this.scroll.direction === 'left') {
         const x = media.mesh.position.x + scaleX
 
         if (x < - this.sizes.width / 2) {
           media.extra.x += this.gallerySizes.width
-          media.mesh.rotation.z = GSAP.utils.random(-Math.PI * 0.03, Math.PI * 0.03)
         }
-      } else if (this.x.direction === 'right') {
+      } else if (this.scroll.direction === 'right') {
         const x = media.mesh.position.x - scaleX
 
         if (x > this.sizes.width / 2) {
           media.extra.x -= this.gallerySizes.width
-          media.mesh.rotation.z = GSAP.utils.random(-Math.PI * 0.03, Math.PI * 0.03)
-        }
-      }
-
-      const scaleY = media.mesh.scale.y / 2
-
-      if (this.y.direction === 'top') {
-        const y = media.mesh.position.y + scaleY
-
-        if (y < - this.sizes.height / 2) {
-          media.extra.y += this.gallerySizes.height
-          media.mesh.rotation.z = GSAP.utils.random(-Math.PI * 0.03, Math.PI * 0.03)
-        }
-      } else if (this.y.direction === 'bottom') {
-        const y = media.mesh.position.y - scaleY
-
-        if (y > this.sizes.width / 2) {
-          media.extra.y -= this.gallerySizes.height
-          media.mesh.rotation.z = GSAP.utils.random(-Math.PI * 0.03, Math.PI * 0.03)
         }
       }
 
