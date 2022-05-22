8a84f4a036641e8edbf78f6308a7c5467f871c22
kowalsking <kovvalsking@gmail.com>
2022-05-22 23:14:53 +0300

    Add some distortion from another project

--- app/shaders/home-vertex.glsl
+++ app/shaders/home-vertex.glsl
@@ -13,7 +13,8 @@ void main() {
 
   vec4 newPosition = modelViewMatrix * vec4(position, 1.0);
 
-  newPosition.z += cos(position.x * PI);
+  // newPosition.z += sin(position.y / uViewportSizes.y * PI + PI / 2.0) * abs(uStrength);
+  newPosition.z += sin(position.y / uViewportSizes.y * PI + PI / 2.0);
 
   gl_Position = projectionMatrix * newPosition;
 }
