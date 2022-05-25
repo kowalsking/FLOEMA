7cb1419a59f170391054a91e3c1a9122fbe1f523
kowalsking <kovvalsking@gmail.com>
2022-05-25 16:46:23 +0300

    Try to fix some bug

--- app/components/Canvas/Home/index.js
+++ app/components/Canvas/Home/index.js
@@ -155,36 +155,38 @@ export default class {
 
     map(this.medias, media => {
       const scaleX = media.mesh.scale.x / 2
+      const offsetX = this.sizes.width * 0.6
 
       if (this.x.direction === 'left') {
         const x = media.mesh.position.x + scaleX
 
-        if (x < - this.sizes.width / 2) {
+        if (x < -offsetX) {
           media.extra.x += this.gallerySizes.width
           media.mesh.rotation.z = GSAP.utils.random(-Math.PI * 0.03, Math.PI * 0.03)
         }
       } else if (this.x.direction === 'right') {
         const x = media.mesh.position.x - scaleX
 
-        if (x > this.sizes.width / 2) {
+        if (x > offsetX) {
           media.extra.x -= this.gallerySizes.width
           media.mesh.rotation.z = GSAP.utils.random(-Math.PI * 0.03, Math.PI * 0.03)
         }
       }
 
       const scaleY = media.mesh.scale.y / 2
+      const offsetY = this.sizes.height * 0.6
 
       if (this.y.direction === 'top') {
         const y = media.mesh.position.y + scaleY
 
-        if (y < - this.sizes.height / 2) {
+        if (y < -offsetY) {
           media.extra.y += this.gallerySizes.height
           media.mesh.rotation.z = GSAP.utils.random(-Math.PI * 0.03, Math.PI * 0.03)
         }
       } else if (this.y.direction === 'bottom') {
         const y = media.mesh.position.y - scaleY
 
-        if (y > this.sizes.width / 2) {
+        if (y > offsetY) {
           media.extra.y -= this.gallerySizes.height
           media.mesh.rotation.z = GSAP.utils.random(-Math.PI * 0.03, Math.PI * 0.03)
         }
