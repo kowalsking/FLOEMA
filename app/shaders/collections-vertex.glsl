3bd44c893f1662d0da5a9bbb539864ab4771565b
kowalsking <kovvalsking@gmail.com>
2022-05-29 00:28:14 +0300

    Some changes in fragments

--- app/shaders/collections-vertex.glsl
+++ app/shaders/collections-vertex.glsl
@@ -4,9 +4,15 @@ attribute vec3 position;
 uniform mat4 modelViewMatrix;
 uniform mat4 projectionMatrix;
 
+varying vec4 vPosition;
 varying vec2 vUv;
 
 void main() {
   vUv = uv;
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+
+  vec4 newPosition = modelViewMatrix * vec4(position, 1.0)
+
+  vPosition = newPosition;
+
+  gl_Position = projectionMatrix * newPosition;
 }
