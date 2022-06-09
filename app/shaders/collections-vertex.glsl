92ede38ab0303b501a08df1bd0163c39a541d112
kowalsking <kovvalsking@gmail.com>
2022-06-09 23:10:52 +0300

    Fix typo

--- app/shaders/collections-vertex.glsl
+++ app/shaders/collections-vertex.glsl
@@ -10,7 +10,7 @@ varying vec2 vUv;
 void main() {
   vUv = uv;
 
-  vec4 newPosition = modelViewMatrix * vec4(position, 1.0)
+  vec4 newPosition = modelViewMatrix * vec4(position, 1.0);
 
   vPosition = newPosition;
 
